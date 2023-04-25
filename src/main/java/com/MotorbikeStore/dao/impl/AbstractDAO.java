c996907ad9a0f24aac0625c4de7563c7a905dee6
trucvy200200 <toan9400@gmail.com>
2023-04-25 11:42:08 +0700

    commit

--- src/main/java/com/MotorbikeStore/dao/impl/AbstractDAO.java
+++ src/main/java/com/MotorbikeStore/dao/impl/AbstractDAO.java
@@ -13,168 +13,139 @@ import java.util.List;
 import com.MotorbikeStore.dao.impl.DBConnection;
 import com.MotorbikeStore.dao.GenericDAO;
 import com.MotorbikeStore.mapper.RowMapper;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AbstractDAO<T> implements GenericDAO<T> {
 
-    DBConnection connect = DBConnection.getInstance();
-    //	protected Connection getConnection() {
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//			String url = "jdbc:mysql://localhost:3306/web_sale_morcycles_ver2";
-//			String user ="root";
-//			String passWord ="Zingzang2";
-//			return DriverManager.getConnection(url ,user, passWord);
-//		} catch (ClassNotFoundException |SQLException e) {
-//			
-//			return null;
-//		} 
-//		
-//	}
+    Connection conn = DBConnection.getInstance().getConnection();
 
     @Override
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        List<T> results = new ArrayList<>();
-//		Connection connect = null;
-//		PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try {
-            //connect = getConnection();
-            //statement = connect.prepareStatement(sql);
-            //set parameter 
-            //setParameter(statement, parameters );
-
-            resultSet = connect.executeQuery(sql, parameters);
-            while (resultSet.next()) {
-                results.add(rowMapper.mapRow(resultSet));
-            }
-            return results;
-        } catch (SQLException e) {
-            return null;
-        }
-//		finally {
-//			try {
-//				if(connect != null) {
-//					connect.close();
-//				}
-//				if(statement != null) {
-//					statement.close();
-//				}
-//				if(resultSet != null) {
-//					resultSet.close();
-//				}
-//			}catch(SQLException e) {
-//				return null;
-//			}
-//		}
-
-    }
-
-//	
-//	private void setParameter(PreparedStatement statement, Object... parameters) {
-//		try {
-//			for(int i = 0; i < parameters.length; i++) {
-//				Object parameter = parameters[i]; 
-//				int index = i +1;
-//				if(parameter instanceof Long ) {
-//					statement.setLong(index, (Long)parameter);
-//				}else if(parameter instanceof Integer) {
-//					statement.setInt(index, (Integer)parameter );
-//				}else if(parameter instanceof String) {
-//					statement.setString(index, (String)parameter);
-//				}else if(parameter instanceof java.sql.Date) {
-//					statement.setDate(index, (java.sql.Date)parameter);
-//				}else if(parameter instanceof Float) {
-//					statement.setFloat(index, (Float)parameter);
-//				}else if(parameter instanceof Timestamp) {
-//					statement.setTimestamp(index, (Timestamp)parameter);
-//				}else if(parameter == null) {
-//					statement.setNull(index, Types.NULL);
-//				}
-//				
-//			}
-//		}catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		
-//	}
-    public void update(String sql, Object... parameters) {
-
-//		Connection connect = null;
-//		PreparedStatement statement = null;
-        try {
-//			connect = getConnection();
-//			connect.setAutoCommit(false);
-//			statement = connect.prepareStatement(sql);
-//			setParameter(statement, parameters);
-            //statement.executeUpdate();
-            //connect.commit();
-            connect.executeUpdate(sql, parameters);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-//		}finally {
-//			try {
-//				if(connect != null) {
-//					connect.close();
-//				}
-//				if(statement != null) {
-//					statement.close();
-//				}
-//				
-//			}catch(SQLException e2) {
-//				e2.printStackTrace();
-//			}
-//		}
-//		
-
-
-public int insert(String sql, Object... parameters) {
-		ResultSet resultSet = null;
-//		Connection connect = null;
-//		
-//		PreparedStatement statement = null;
-		try {
-			int id = 0;
-//			connect = getConnection();
-//			connect.setAutoCommit(false);
-			//statement = connect.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			//setParameter(statement, parameters);
-			//statement.executeUpdate();
-			resultSet = connect.insert(sql,parameters);
-			if(resultSet.next()) {
-				id= resultSet.getInt(1);
-			}
-			//connect.commit();
-			return id;
-		}catch(SQLException e) {
-//			if(connect != null) {
-//				try {
-//					connect.rollback();
-//				} catch (SQLException e1) {
-					
-					e.printStackTrace();
-//				}
-			}
-			
-			
-//		}finally {
-//			try {
-//				if(connect != null) {
-//					connect.close();
-//				}
-//				if(statement != null) {
-//					statement.close();
-//				}
-//				if(resultSet != null) {
-//					resultSet.close();
-//				}
-//			}catch(SQLException e2) {
-//				e2.printStackTrace();
-//			}
-//		}
-		return 0;
+	public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+		List<T> result = new ArrayList<>();
 		
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;            
+		try {
+			statement = conn.prepareStatement(sql);
+			setParameter(statement, parameters);
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				result.add(rowMapper.mapRow(resultSet));
+			}
+			return result;
+		} catch (SQLException e) {
+			return null;
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (SQLException e) {
+				return null;
+			}
+		}
+	}
+
+	private void setParameter(PreparedStatement statement, Object... parameters) {
+		try {
+			for (int i = 0; i < parameters.length; i++) {
+				Object parameter = parameters[i];
+				int index = i + 1;
+				if (parameter instanceof Long) {
+					statement.setLong(index, (Long) parameter);
+				} else if (parameter instanceof String) {
+					statement.setString(index, (String) parameter);
+				} else if (parameter instanceof Integer) {
+					statement.setInt(index, (Integer) parameter);
+				} else if (parameter instanceof Timestamp) {
+					statement.setTimestamp(index, (Timestamp) parameter);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void update(String sql, Object... parameters) {
+		PreparedStatement statement = null;
+		try {
+			
+			conn.setAutoCommit(false);
+			statement = conn.prepareStatement(sql);
+			setParameter(statement, parameters);
+			statement.executeUpdate();
+			conn.commit();
+		} catch (SQLException e) {
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public int insert(String sql, Object... parameters) {
+		ResultSet rs = null;
+	
+		PreparedStatement statement = null;
+		try {
+			Integer id = null;
+			
+			conn.setAutoCommit(false);
+			statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			setParameter(statement, parameters);
+			statement.executeUpdate();
+			rs = statement.getGeneratedKeys();
+			if (rs.next()) {
+				id = rs.getInt(1);
+			}
+			conn.commit();
+			return id;
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+		return 0;
 	}
 }
